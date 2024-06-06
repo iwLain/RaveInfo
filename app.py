3722c5e471c0c64a13e108c086ef6f0bdced60cf
FinnLab <95759983+iwLain@users.noreply.github.com>
2024-06-06 20:06:43 +0000

    added PW protection

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import configparser
-from datetime import datetime
+from datetime import datetime, timedelta
+import pytz
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash
 from werkzeug.utils import secure_filename
 from collections import OrderedDict
@@ -66,14 +67,21 @@ def schedule():
     except Exception as e:
         flash(f"Error loading DJ schedule: {e}")
         djs = {}
+
     try:
-        now = datetime.now().time()
-        current_dj = next((dj for dj, details in djs.items() if datetime.strptime(details['time'], '%H:%M').time() <= now), None)
+        berlin_tz = pytz.timezone('Europe/Berlin')
+        now = datetime.now(berlin_tz).time()
+        current_dj = None
+        for dj, details in djs.items():
+            dj_time = datetime.strptime(details['time'], '%H:%M').time()
+            if dj_time <= now:
+                current_dj = dj
         progress = len([dj for dj, details in djs.items() if datetime.strptime(details['time'], '%H:%M').time() <= now]) / len(djs) if djs else 0
     except Exception as e:
         flash(f"Error calculating current DJ: {e}")
         current_dj = None
         progress = 0
+
     return render_template('schedule.html', djs=djs, current_dj=current_dj, progress=progress)
 
 @app.route('/location')
